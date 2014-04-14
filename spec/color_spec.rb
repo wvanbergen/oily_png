a647cb4889367d203b972b7177571eea2cdd1680
Joe Lencioni <joe.lencioni@causes.com>
2014-04-14 07:55:02 -0700

    Clean up color_spec.rb
    Converted double quotes to single quotes and removed some unwanted
    whitespace.

--- spec/color_spec.rb
+++ spec/color_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe OilyPNG::Color do
-
   include OilyPNG::Color
 
   before(:each) do
@@ -13,24 +12,23 @@ describe OilyPNG::Color do
   end
 
   describe '#compose_quick' do
-
-    it "should use the foregorund color as is when the background color is fully transparent" do
+    it 'should use the foregorund color as is when the background color is fully transparent' do
       compose_quick(@non_opaque, @fully_transparent).should == @non_opaque
     end
 
-    it "should use the foregorund color as is when an opaque color is given as foreground color" do
+    it 'should use the foregorund color as is when an opaque color is given as foreground color' do
       compose_quick(@opaque, @white).should == @opaque
     end
 
-    it "should use the background color as is when a fully transparent pixel is given as foreground color" do
+    it 'should use the background color as is when a fully transparent pixel is given as foreground color' do
       compose_quick(@fully_transparent, @white).should == @white
     end
 
-    it "should compose pixels correctly" do
+    it 'should compose pixels correctly' do
       compose_quick(@non_opaque, @white).should == 0x9fc2d6ff
     end
-    
-    it "should compose colors exactly the same as ChunkyPNG" do
+
+    it 'should compose colors exactly the same as ChunkyPNG' do
       fg, bg = rand(0xffffffff), rand(0xffffffff)
       compose_quick(fg, bg).should == ChunkyPNG::Color.compose_quick(fg, bg)
     end
